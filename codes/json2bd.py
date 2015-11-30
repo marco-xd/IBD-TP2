c57473f55bf6deee51e47d89f99c017d09492765
Marco <marcoaremail@gmail.com>
2015-11-30 19:23:01 -0200

    Outra tentativa de processar utf8mb4

--- codes/json2bd.py
+++ codes/json2bd.py
@@ -128,7 +128,7 @@ def insertPlayerStats (db):
 		myId = jName[:-5]
 		for gId, info in jzim.iteritems():
 			for aux in info:
-				print "\r" + myId + ' ' + aux['name'] + ' ' + str(aux['value']),
+				print "\r" + (' ' * 80) + "\r" + myId + ' ' + aux['name'] + ' ' + str(aux['value']),
 				cursor.execute("INSERT INTO playerstats(steamid, appid, stat_name, value) VALUES (%s, %s, %s, %s)",
 					(myId, gId, aux['name'], aux['value']))
 	exit(0)
